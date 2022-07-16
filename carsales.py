fc9ba535d4d6a820123f09ce04ac2ec7d036623d
Ayokunle Olowoniwa <Ayokunle.Olowoniwa@etranzact.com>
2022-07-16 16:59:05 +0100

    Added JWT implementation

--- carsales.py
+++ carsales.py
@@ -1,5 +1,8 @@
+from enum import unique
+from functools import wraps
 import os
 from flask import Flask, redirect, render_template, request, jsonify, abort
+import jwt
 import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
@@ -9,16 +12,17 @@ load_dotenv()
 # create .env file and setup secretes
 user = os.getenv('u_name')
 passwd = os.getenv('password')
+secret =  os.getenv('secret')
 carsales = Flask(__name__)
 
 CORS(carsales)
 
-carsales.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{passwd}@localhost/vehicles'
+carsales.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{passwd}@localhost:5433/vehicles'
 carsales.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(carsales)
 
-
+#Car Model
 class RedCars(db.Model):
     __tablename__ = "redcars"
     id = db.Column(db.Integer, primary_key=True)
@@ -32,9 +36,49 @@ class RedCars(db.Model):
     def __repr__(self):
         return "<RedCars %r>" % self.car_name
 
+#User Model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100))
+
 
 db.create_all()
 
+
+#decorator for verifying jwt
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        #check if jwt is passed in request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        
+        if not token:
+            return jsonify({
+                "message": "hey there!, your token is missing!!!"
+            }), 401
+            
+        try:
+            #decode the payload to fetch the stored details
+            data = jwt.decode(token, secret)
+            current_user = User.query.filter_by(public_id = data['public_id']).first()
+        
+        except:
+            return jsonify({
+                "message": "Hey there, your token is invalid!"
+            }), 401
+
+        #returns the current logged in user to the route
+        return f(current_user, *args, **kwargs)
+    
+    return decorated
 
 @carsales.route('/')
 def index():
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Projects\Python\RedDevils\env
+set VIRTUAL_ENV=C:\Users\ayokunle.olowoniwa\Downloads\gaby\udacity\Projects\reddevils\UdacityNanodegree_RedDevilsClass\env
 
 if not defined PROMPT set PROMPT=$P$G
 
